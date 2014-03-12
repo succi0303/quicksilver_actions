323f33e53824d167bdf24fff92c33d57efca9968
succi0303 <succi0303@gmail.com>
2014-03-13 06:01:44 +0900

    add yardoc comment

--- actions/uri_decode.rb
+++ actions/uri_decode.rb
@@ -3,14 +3,29 @@
 
 require 'uri'
 
+# 
+# == URIDecode
+#
+# Quicksilverの1stペインのテキストをURIデコードし
+# 1stペインに書き戻す。
+#
+# @author succi0303
 class URIDecode
 
+  # 引数のテキストをURIデコードした後、シェル実行用のコマンドを作成し
+  # systemコマンドで実行する
+  #
+  # @param [String] word デコード対象のテキスト
   def decode(word)
     decoded_word = uri_decode(word)
     cmd = construct_command(decoded_word)
     system cmd
   end
 
+  # シェル実行用のコマンドを組み立てる
+  #
+  # @param [String] word デコード済みのテキスト
+  # @return [String] シェル実行用のコマンド
   def construct_command(word)
     quoted_word = "'#{word}'"
     base_cmd_before = 'echo'
@@ -19,6 +34,10 @@ class URIDecode
     cmd.join(' ')
   end
 
+  # URIデコードする
+  #
+  # @param [String] word デコード対象のテキスト
+  # @return [String] デコード済みテキスト
   def uri_decode(word)
     decoded_word = URI.decode(word).downcase
     return decoded_word
--- actions/uri_encode.rb
+++ actions/uri_encode.rb
@@ -3,14 +3,29 @@
 
 require 'uri'
 
+# 
+# == URIEncode
+#
+# Quicksilverの1stペインのテキストをURIエンコードし
+# 1stペインに書き戻す
+# 
+# @author succi0303
 class URIEncode
 
+  # 引数のテキストをURIエンコードした後、シェル実行用のコマンドを作成し
+  # systemコマンドで実行する。
+  #
+  # @param [String] word エンコード対象のテキスト
   def encode(word)
     encoded_word = uri_encode(word)
     cmd = construct_command(encoded_word)
     system cmd
   end
 
+  # シェル実行用のコマンドを組み立てる
+  #
+  # @param [String] word エンコード済みのテキスト
+  # @return [String] シェル実行用のコマンド
   def construct_command(word)
     quoted_word = "'#{word}'"
     base_cmd_before = 'echo'
@@ -19,6 +34,10 @@ class URIEncode
     cmd.join(' ')
   end
 
+  # URIエンコードする
+  #
+  # @param [String] word エンコード対象のテキスト
+  # @return [String] エンコード済みテキスト
   def uri_encode(word)
     encoded_word = URI.encode(word).downcase
     return encoded_word
