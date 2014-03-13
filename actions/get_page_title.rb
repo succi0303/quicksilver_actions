7acf3315bd0c00e7aeebd4405e86d763f6a5cdd3
succi0303 <succi0303@gmail.com>
2014-03-13 18:21:39 +0900

    add yardoc comment

--- actions/get_page_title.rb
+++ actions/get_page_title.rb
@@ -4,8 +4,19 @@
 require 'open-uri'
 require 'nokogiri'
 
+# 
+# == GetPageTitle
+#
+# Quicksilverの1stペインに入力しURLから
+# 該当のページのタイトルを取得し、1stペインに書き戻す
+#
+# @auhor succi0303
 class GetPageTitle
 
+  # URLを受取り、該当するページのタイトルを返す
+  #
+  # @param [String] url タイトル取得対象のURL
+  # @note HTMLの取得に失敗した場合、Quicksilverの1stペインにエラーメッセージを出力する
   def get_title(url)
     begin
       html = get_html_from_url(url)
@@ -22,6 +33,10 @@ class GetPageTitle
 
   private
 
+  # シェル実行用のコマンドを組み立てる
+  #
+  # @param [String] string Quicksilverに出力する文字列
+  # @return [String] シェル実行用のコマンド
   def construct_command(string)
     quoted_string = %Q("#{string}")
     cmd_before = 'echo'
@@ -30,11 +45,19 @@ class GetPageTitle
     cmd.join(' ')
   end
 
+  # 該当するURLのHTMLボディを取得する
+  #
+  # @param [String] url タイトル取得対象のURL
+  # @return [String] 該当するURLのHTMLボディ
   def get_html_from_url(url)
     html = open(url).read
     return html
   end
 
+  # HTMLからタイトルを取得する
+  #
+  # @param [String] HTMLボディ
+  # @return [String] HTMLのtitle
   def get_title_from_html(html)
     page = Nokogiri::HTML(html)
     page.title
