bc6f317f09b89edb178b7f2dae151660fee69b0e
succi0303 <succi0303@gmail.com>
2014-03-14 15:18:11 +0900

    refactor

--- actions/uri_encode.rb
+++ actions/uri_encode.rb
@@ -3,54 +3,58 @@
 
 require 'uri'
 
-# 
-# == URIEncode
-#
-# Quicksilverの1stペインのテキストをURIエンコードし
-# 1stペインに書き戻す
-# 
-# @author succi0303
-class URIEncode
+module QSActions
 
-  # 引数のテキストをURIエンコードした後、シェル実行用のコマンドを作成し
-  # systemコマンドで実行する。
-  #
-  # @param [String] word エンコード対象のテキスト
-  def encode(word)
-    encoded_word = uri_encode(word)
-    cmd = construct_command(encoded_word)
-    system cmd
+  def self.this_is_test?
+    !($0 == __FILE__)
   end
 
-  private
+  # @author succi0303
+  class URIEncode
 
-  # シェル実行用のコマンドを組み立てる
-  #
-  # @param [String] word エンコード済みのテキスト
-  # @return [String] シェル実行用のコマンド
-  def construct_command(word)
-    quoted_word = "'#{word}'"
-    base_cmd_before = 'echo'
-    base_cmd_after =  '| qs'
-    cmd = [base_cmd_before, quoted_word, base_cmd_after]
-    cmd.join(' ')
-  end
+    # Quicksilverの1stペインのテキストをURIエンコードし
+    # 1stペインに書き戻す
+    #
+    # @param [String] text Quicksilver 1stペインのテキスト
+    def execute_action(text)
+      updated_text = update_text(text)
+      output_into_quicksilver(updated_text)
+    end
 
-  # URIエンコードする
-  #
-  # @param [String] word エンコード対象のテキスト
-  # @return [String] エンコード済みテキスト
-  def uri_encode(word)
-    encoded_word = URI.encode(word).downcase
-    return encoded_word
+    private
+
+    # テキストをQuicksilverに出力する
+    #
+    # @param [String] text Quicksilverに出力するテキスト
+    def output_into_quicksilver(word)
+      quoted_word = %Q("#{word}")
+      base_cmd_before = 'echo'
+      base_cmd_after =  '| qs'
+
+      cmd_array = [base_cmd_before, quoted_word, base_cmd_after]
+      cmd = cmd_array.join(' ')
+
+      return cmd if QSActions::this_is_test?
+      system cmd
+    end
+
+    # URIエンコードする
+    #
+    # @param [String] word エンコード対象のテキスト
+    # @return [String] エンコード済みテキスト
+    def update_text(text)
+      encoded_text = URI.encode(text).downcase
+      return encoded_text
+    end
+
   end
 
 end
 
-if $0 == __FILE__
+unless QSActions::this_is_test?
 
-  word = ARGV[0]
-  ue = URIEncode.new
-  ue.encode(word)
+  text = ARGV[0]
+  ue = QSActions::URIEncode.new
+  ue.execute_action(text)
 
 end
--- specs/uri_encode_spec.rb
+++ specs/uri_encode_spec.rb
@@ -2,15 +2,15 @@ require_relative '../actions/uri_encode'
 
 describe "URIEncode" do
   
-  let(:ue) { URIEncode.new }
-
-  it "construct correct command" do
-    cmd = ue.send(:construct_command, 'hogehoge')
-    expect(cmd).to eq "echo 'hogehoge' | qs"
-  end
+  let(:ue) { QSActions::URIEncode.new }
 
   it "does uriencode for args" do
-    encoded_word = ue.send(:uri_encode, 'テスト')
-    expect(encoded_word).to eq '%e3%83%86%e3%82%b9%e3%83%88'
+    encoded_text = ue.send(:update_text, 'テスト')
+    expect(encoded_text).to eq '%e3%83%86%e3%82%b9%e3%83%88'
+  end
+
+  it "output result into quicksilver" do
+    cmd = ue.send(:output_into_quicksilver, 'hogehoge')
+    expect(cmd).to eq 'echo "hogehoge" | qs'
   end
 end
