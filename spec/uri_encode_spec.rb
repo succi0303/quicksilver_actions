f6b539aecaf9a98035673834dfe1cca5e0ae132f
succi0303 <succi0303@gmail.com>
2014-03-24 21:42:46 +0900

    refactor 'uri_encode'

--- spec/uri_encode_spec.rb
+++ spec/uri_encode_spec.rb
@@ -2,10 +2,10 @@ require 'spec_helper'
 
 describe "URIEncode" do
   
-  let(:ue) { QSActions::URIEncode.new }
+  let(:action) { QSActions::URIEncode.new }
 
   it "does uriencode for args" do
-    encoded_text = ue.send(:update_text, 'テスト')
-    expect(encoded_text).to eq '%e3%83%86%e3%82%b9%e3%83%88'
+    edited_text = action.send(:edit_text, 'テスト')
+    expect(edited_text).to eq '%e3%83%86%e3%82%b9%e3%83%88'
   end
 end
