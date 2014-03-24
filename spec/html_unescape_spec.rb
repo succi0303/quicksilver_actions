6074be9f0d6f700ad2ae4056a21065d57f29612a
succi0303 <succi0303@gmail.com>
2014-03-24 21:39:03 +0900

    refacotr 'html_unescape'

--- spec/html_unescape_spec.rb
+++ spec/html_unescape_spec.rb
@@ -2,10 +2,10 @@ require 'spec_helper'
 
 describe "html_unescape" do
 
-  let(:hu) { QSActions::HTMLUnescape.new }
+  let(:action) { QSActions::HTMLUnescape.new }
 
   it "does html unescape correctly" do
-    unescaped_word = hu.send(:update_text, '&lt;html&gt;&lt;/html&gt;')
-    expect(unescaped_word).to eq "<html></html>"
+    edited_text = action.send(:edit_text, '&lt;html&gt;&lt;/html&gt;')
+    expect(edited_text).to eq "<html></html>"
   end
 end
